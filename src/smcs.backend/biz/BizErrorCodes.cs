4c8ca9a7400fec7e0473852a3ae4acf69399d01f
marvel <mrv@localhost>
2019-07-13 00:11:23 +0430

    ساختار نتیجه بازگردانده شده از عملیات تغییر کرد و  موارد مناسب افزوده شد

--- src/smcs.backend/biz/BizErrorCodes.cs
+++ src/smcs.backend/biz/BizErrorCodes.cs
@@ -1,28 +1,57 @@
-﻿using System;
-
-public class BizErrCod
+﻿public class BizErrCod
 {
-    public readonly static int DB_UPDT_FAIL = -101;
-    public readonly static int DB_INS_FAIL = -100;
-                               
-    public readonly static int AGNT_ABS_DUTY_CONF = -11;
-    public readonly static int AGNT_OFF_DUTY_CONF = -10;
-    public readonly static int AGNT_OFF_ABS_CONF = -9;
-                               
-    public readonly static int SESI_NOT_EXST = -8;
-    public readonly static int ONDUTY_NOT_EXST = -7;
-    public readonly static int ABS_NOT_EXST = -6;
-    public readonly static int OFF_NOT_EXST = -5;
-    public readonly static int MIS_NOT_EXST = -4;
-    public readonly static int AGNT_NOT_EXST = -3;
-    public readonly static int USR_NOT_EXST = -2;
-                               
-    public readonly static int OPR_FAIL = -1;
+    public readonly static Message UPDT_SIGN_SUCC = Message.Succ("بروزرسانی امضا");
+    public readonly static Message EXTN_MIS_SUCC  = Message.Succ("تمدید ماوریت");
+    public readonly static Message SIGN_UPDT_FAIL= Message.Fail("بروزرسانی امضا");
+    public readonly static Message EXTN_MIS_FAIL = Message.Fail("تمدید ماموریت");
+                           
+    public readonly static Message OFF_UNDT_ABS_UNTRT_CONF = Message.AlreadyExist("گردش‌گار دیگری");
+    public readonly static Message OPR_FAIL = Message.Fail("ثبت گردش‌کار");
+                           
+    public readonly static Message SESI_NOT_EXST = Message.AlreadyRemoved("نشست انتخاب شده");
+                           
+    public readonly static Message AG_REM_ITER_SUCC = Message.Succ("حذف گردشکار");
+    public readonly static Message WRT_AG_ITER_SUCC = Message.Succ("ثبت گردشکار");
+                           
+    public readonly static Message RMV_UNTR_FAIL = Message.Fail("حذف اعزام‌به‌بهداری");
+    public readonly static Message WRT_UNTRT_FAIL= Message.Fail("ثبت اعزام‌به‌بهداری");
+                           
+    public readonly static Message RMV_ABS_FAIL = Message.Fail("حذف نهست");
+    public readonly static Message WRT_ABS_FAIL = Message.Fail("ثبت نهست");
+    public readonly static Message ABS_NOT_EXST = Message.AlreadyRemoved("نهست انتخاب شده");
+                           
+    public readonly static Message RMV_ONDUT_FAIL = Message.Fail("حذف امورخدمتی");
+    public readonly static Message WRT_ONDUT_FAIL = Message.Fail("ثبت امورخدمتی");
+    public readonly static Message ONDUT_NOT_EXST = Message.AlreadyRemoved("امورخدمتی انتخاب شده");
+                           
+    public readonly static Message RMV_OFF_FAIL = Message.Fail("حذف مرخصی");
+    public readonly static Message WRT_OFF_FAIL = Message.Fail("ثبت مرخصی");
+    public readonly static Message OFF_NOT_EXST = Message.AlreadyRemoved("مرخصی انتخاب شده قبل‌تر از سیستم حذف شده");
+                           
+    public readonly static Message AG_OFC_REM_SUCC = Message.Succ("ثبت قسمت مامور");
+    public readonly static Message AG_REG_OFC_SUCC = Message.Succ("حذف قسمت مامور");
+    public readonly static Message AG_OFC_REM_FAIL = Message.Fail("حذف قسمت مامور");
+    public readonly static Message AG_OFC_REG_FAIL = Message.Fail("ثبت قسمت مامور");
+                           
+    public readonly static Message AG_DISM_SUCC = Message.Succ("ثبت پایان ماموریت");
+    public readonly static Message AG_DISM_FAIL = Message.Fail("ثبت پایان ماموریت");
+    public readonly static Message MIS_NOT_EXST = Message.AlreadyRemoved("ماموریت");
+                           
+    public readonly static Message AG_N_MIS_UPDT_SUCC = Message.Succ("بروزرسانی مامور و ماوریت او");
+    public readonly static Message AG_N_MIS_UPDT_FAIL = Message.Fail("بروزرسانی مامور و ماوریت او");
 
-    public readonly static int OPR_SUCC = 0;
-                               
-    public readonly static int AGNT_ALRDY_DISSMSD = 1;
-    public readonly static int AGNT_ALRDY_HAS_OFF  = 2;
-    public readonly static int AGNT_ALRDY_HAS_ABS  = 3;
-    public readonly static int AGNT_ALRDY_HAS_DUTY = 4;
+    public readonly static Message AG_UPDT_SUCC = Message.Succ("بروزرسانی مشخصات مامور");
+    public readonly static Message AG_UPDT_FAIL = Message.Fail("بروزرسانی مشخصات مامور");
+    public readonly static Message AG_REG_SUCC = Message.Succ("پذیرش مامور");
+    public readonly static Message AG_REG_FAIL = Message.Fail("پذیرش مامور");
+    public readonly static Message AGNT_ALRDY_DISM = Message.AlreadyExist("پایان کاربر یادشده");
+    public readonly static Message AGNT_NOT_EXST = Message.AlreadyRemoved("کاربر انتخاب شده");
+                           
+    public readonly static Message LOG_OUT_FAIL = Message.Fail("خروج از سیستم");
+    public readonly static Message LOG_OUT_SUCC = Message.Succ("خروج از سیستم");
+    public readonly static Message LOG_IN_FAIL = Message.LogInFail("ورود به سیستم امکان‌پذیر نیست؛ احتمالا رمز عبور نامعتبر است");
+    public readonly static Message LOG_IN_SUCC = Message.Succ("خروج از سیستم");
+                           
+    public readonly static Message USR_NOT_EXST = Message.AlreadyRemoved("کاربر انتخاب شده");
+
 }
--- src/smcs.backend/biz/Error.cs
+++ src/smcs.backend/biz/Error.cs
@@ -1,9 +1,38 @@
-﻿public class Error
+﻿public class Message
 {
-    public string Message { get; private set; }
+    public int Id { get; private set; }
 
-    public Error(string message)
+    public string Text { get; private set; }
+
+    private Message(int id, string txt)
     {
-        this.Message = message;
+        this.Id = id;
+        this.Text = txt;
     }
+
+    public static Message LogInFail(string txt)
+    {
+        return new Message(-2, txt);
+    }
+
+    public static Message Fail(string opr)
+    {
+        return new Message(-1, "عملیات " + opr + "با شکست مواجه شد ");
+    }
+
+    public static Message Succ(string opr)
+    {
+        return new Message(0, "عملیات " + opr + "با موفقیت انجام شد ");
+    }
+
+    public static Message AlreadyExist(string target)
+    {
+        return new Message(1, target + "قبل‌تر در سیستم ثبت شده ");
+    }
+
+    public static Message AlreadyRemoved(string target)
+    {
+        return new Message(2, target + "قبل‌تر از سیستم حذف شده ");
+    }
+
 }
