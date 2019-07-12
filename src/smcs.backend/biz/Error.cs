60b1db67067726eebb5513edaac9a4177901c6d8
marvel <mrv@localhost>
2019-07-12 22:11:17 +0430

    کلاس خطای ما فعلا به شناسه نیازی ندارد

--- src/smcs.backend/biz/Error.cs
+++ src/smcs.backend/biz/Error.cs
@@ -1,11 +1,9 @@
 ﻿public class Error
 {
-    public int Id { get; private set; }
     public string Message { get; private set; }
 
-    public Error(int id, string message)
+    public Error(string message)
     {
-        this.Id = id;
         this.Message = message;
     }
 }
