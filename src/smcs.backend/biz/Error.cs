ea61cf41a66b4a879cb17dc4ed8c43a7c7f2f015
marvel <mrv@localhost>
2019-07-12 21:55:40 +0430

    نسخه ابتدایی کلاس خطا؛ صرفا شناسه و پیغام نمایش داده شده،ایجاد شد

---
+++ src/smcs.backend/biz/Error.cs
@@ -0,0 +1,11 @@
+﻿public class Error
+{
+    public int Id { get; private set; }
+    public string Message { get; private set; }
+
+    public Error(int id, string message)
+    {
+        this.Id = id;
+        this.Message = message;
+    }
+}
