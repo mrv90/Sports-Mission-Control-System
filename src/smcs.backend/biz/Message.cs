ade3e411952fd045fb3cb9979f2a1144a5138348
mrv <mrv@win-wks>
2019-08-04 12:30:00 +0430

    فقط کلاس پایه پیغام، عملیات را اعلام می کند. یک رنگ نیز برای نمایش بصری افزوده شد

--- src/smcs.backend/biz/Message.cs
+++ src/smcs.backend/biz/Message.cs
@@ -1,4 +1,7 @@
-﻿public class Message
+﻿using smcs.backend.biz;
+using System.Drawing;
+
+public class Message
 {
     public static readonly int LOG_IN_FAIL = -2;
     public static readonly int FAIL = -1;
@@ -10,34 +13,47 @@
 
     public string Text { get; private set; }
 
-    private Message(int id, string txt)
+    public Color Color { get; private set; }
+
+    private Message(int id, string txt, Color clr)
     {
         this.Id = id;
         this.Text = txt;
+        this.Color = clr;
     }
 
     public static Message LogInFail(string txt)
     {
-        return new Message(-2, txt);
+        var msg = new Message(-2, txt, Color.Red);
+        MessageObserver.Notify(msg);
+        return msg;
     }
 
     public static Message Fail(string opr)
     {
-        return new Message(-1, "عملیات " + opr + "با شکست مواجه شد ");
+        var msg = new Message(-1, "عملیات " + opr + "با شکست مواجه شد ", Color.Red);
+        MessageObserver.Notify(msg);
+        return msg;
     }
 
     public static Message Succ(string opr)
     {
-        return new Message(0, "عملیات " + opr + "با موفقیت انجام شد ");
+        var msg = new Message(0, "عملیات " + opr + "با موفقیت انجام شد ", Color.Green);
+        MessageObserver.Notify(msg);
+        return msg;
     }
 
     public static Message AlreadyExist(string target)
     {
-        return new Message(1, target + "قبل‌تر در سیستم ثبت شده ");
+        var msg = new Message(1, target + "قبل‌تر در سیستم ثبت شده ", Color.LightGray);
+        MessageObserver.Notify(msg);
+        return msg;
     }
 
     public static Message AlreadyRemoved(string target)
     {
-        return new Message(2, target + "قبل‌تر از سیستم حذف شده ");
+        var msg = new Message(2, target + "قبل‌تر از سیستم حذف شده ", Color.LightGray);
+        MessageObserver.Notify(msg);
+        return msg;
     }
 }
