d34ebc1ef90da83c0aa2a3f602966b85c49bcab2
mrv <mrv@win-wks>
2019-08-04 19:33:58 +0430

    نسخه جدید کلاس پیغام؛ توجه شود که پیغامهای خطای بیزنسی حذف شد

--- src/smcs.backend/biz/Message.cs
+++ src/smcs.backend/biz/Message.cs
@@ -1,13 +1,14 @@
 ﻿using smcs.backend.biz;
+using System;
 using System.Drawing;
 
 public class Message
 {
-    public static readonly int LOG_IN_FAIL = -2;
+    public static readonly int NOT_EXST = -2;
     public static readonly int FAIL = -1;
     public static readonly int SUCC = 0;
     public static readonly int ALDY_EXST = 1;
-    public static readonly int ALDY_REMV = 2;
+    public static readonly int CONFLICT = 2;
 
     public int Id { get; private set; }
 
@@ -22,37 +23,38 @@ public class Message
         this.Color = clr;
     }
 
-    public static Message LogInFail(string txt)
+    public static Message Fail(string opr, params string[] args)
     {
-        var msg = new Message(-2, txt, Color.Red);
+        var msg = new Message(FAIL, DateTime.Now.ToShortTimeString() + " - " + " شکست" + opr + "؛ " + args, Color.Red);
         MessageObserver.Notify(msg);
         return msg;
     }
 
-    public static Message Fail(string opr)
+    public static Message Succ(string opr, params string[] args)
     {
-        var msg = new Message(-1, "عملیات " + opr + "با شکست مواجه شد ", Color.Red);
+        var msg = new Message(SUCC, DateTime.Now.ToShortTimeString() + " - " + " عملیات موفق" + opr + "؛ " + args, Color.Green);
+        MessageObserver.Notify(msg);
+        // ثبت عملیات موفق در کارکرد روزانه
+        return msg;
+    }
+
+    public static Message AlreadyExist(string opr, params string[] args)
+    {
+        var msg = new Message(ALDY_EXST, DateTime.Now.ToShortTimeString() + " - " + " عملیات" + opr + " به علت تکراری بودن مقادیر " + args + ".لغو شد ", Color.Black);
         MessageObserver.Notify(msg);
         return msg;
     }
 
-    public static Message Succ(string opr)
+    public static Message NotExist(string opr, params string[] args)
     {
-        var msg = new Message(0, "عملیات " + opr + "با موفقیت انجام شد ", Color.Green);
+        var msg = new Message(NOT_EXST, DateTime.Now.ToShortTimeString() + " - " + " عملیات" + opr + " به علت عدم وجود مقادیر " + args + ".لغو شد ", Color.Black);
         MessageObserver.Notify(msg);
         return msg;
     }
 
-    public static Message AlreadyExist(string target)
+    internal static Message Conflict(string opr, params string[] args)
     {
-        var msg = new Message(1, target + "قبل‌تر در سیستم ثبت شده ", Color.LightGray);
-        MessageObserver.Notify(msg);
-        return msg;
-    }
-
-    public static Message AlreadyRemoved(string target)
-    {
-        var msg = new Message(2, target + "قبل‌تر از سیستم حذف شده ", Color.LightGray);
+        var msg = new Message(CONFLICT, DateTime.Now.ToShortTimeString() + " - " + " عملیات" + opr + " به علت مغایرت و همپوشانی مقادیر " + args + ".لغو شد ", Color.Black);
         MessageObserver.Notify(msg);
         return msg;
     }
