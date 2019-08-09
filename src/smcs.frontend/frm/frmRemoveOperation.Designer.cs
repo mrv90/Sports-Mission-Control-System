c5fa15a9d83f3cc6b717c36f40c1e5d14776eafd
mrv <mrv@win-wks>
2019-08-09 12:14:42 +0430

    نسخه دوم فرم لغو عملیات؛ کاملا تست شده و کار میکند. توجه شود که طراحی از لغو عملیات روزانه به لغو عملیات بر مبنای کلا عملیات های مامور تغییر یافت.

--- src/smcs.frontend/frm/frmRemoveOperation.Designer.cs
+++ src/smcs.frontend/frm/frmRemoveOperation.Designer.cs
@@ -29,17 +29,59 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lstOpr = new System.Windows.Forms.ListView();
-            this.hdrApplyDT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdrEnt = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdrAgName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdrTrgtDT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.hdrDesc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cmuListView = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.tspRemHistItm = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnUpd = new System.Windows.Forms.Button();
+            this.tsmiCancelItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbSearch = new System.Windows.Forms.ComboBox();
+            this.lstOpr = new System.Windows.Forms.ListView();
+            this.hdrId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdrTS = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdrOpr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdrTrg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdrUsr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.hdrDesc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.cmuListView.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // cmuListView
+            // 
+            this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiCancelItem});
+            this.cmuListView.Name = "cmuWholeForm";
+            this.cmuListView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cmuListView.Size = new System.Drawing.Size(116, 26);
+            // 
+            // tsmiCancelItem
+            // 
+            this.tsmiCancelItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsmiCancelItem.Name = "tsmiCancelItem";
+            this.tsmiCancelItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.tsmiCancelItem.Size = new System.Drawing.Size(115, 22);
+            this.tsmiCancelItem.Text = "لغو";
+            this.tsmiCancelItem.Click += new System.EventHandler(this.tsmiCancelItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.cmbSearch);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(714, 69);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // cmbSearch
+            // 
+            this.cmbSearch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cmbSearch.Location = new System.Drawing.Point(3, 30);
+            this.cmbSearch.Name = "cmbSearch";
+            this.cmbSearch.Size = new System.Drawing.Size(708, 34);
+            this.cmbSearch.TabIndex = 24;
+            this.cmbSearch.TextChanged += new System.EventHandler(this.cmbSearch_TextChanged);
+            this.cmbSearch.KeyUp += new System.Windows.Forms.KeyEventHandler(this.cmbSearch_KeyUp);
             // 
             // lstOpr
             // 
@@ -47,107 +89,92 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstOpr.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.hdrApplyDT,
-            this.hdrEnt,
-            this.hdrAgName,
-            this.hdrTrgtDT,
+            this.hdrId,
+            this.hdrTS,
+            this.hdrOpr,
+            this.hdrTrg,
+            this.hdrUsr,
             this.hdrDesc});
             this.lstOpr.ContextMenuStrip = this.cmuListView;
-            this.lstOpr.Location = new System.Drawing.Point(0, 1);
+            this.lstOpr.FullRowSelect = true;
+            this.lstOpr.HideSelection = false;
+            this.lstOpr.Location = new System.Drawing.Point(3, 70);
             this.lstOpr.MultiSelect = false;
             this.lstOpr.Name = "lstOpr";
-            this.lstOpr.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.lstOpr.Size = new System.Drawing.Size(569, 493);
-            this.lstOpr.TabIndex = 0;
+            this.lstOpr.Size = new System.Drawing.Size(708, 473);
+            this.lstOpr.TabIndex = 2;
             this.lstOpr.UseCompatibleStateImageBehavior = false;
             this.lstOpr.View = System.Windows.Forms.View.Details;
             // 
-            // hdrApplyDT
+            // hdrId
             // 
-            this.hdrApplyDT.Text = "تاریخ ثبت";
-            this.hdrApplyDT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.hdrApplyDT.Width = 95;
+            this.hdrId.DisplayIndex = 5;
+            this.hdrId.Text = "ردیف";
+            this.hdrId.Width = 50;
             // 
-            // hdrEnt
+            // hdrTS
             // 
-            this.hdrEnt.Text = "ن‌عملیات";
-            this.hdrEnt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.hdrEnt.Width = 104;
+            this.hdrTS.DisplayIndex = 4;
+            this.hdrTS.Text = "تاریخ‌وزمان اِعمال";
+            this.hdrTS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hdrTS.Width = 200;
             // 
-            // hdrAgName
+            // hdrOpr
             // 
-            this.hdrAgName.Text = "نام مامور";
-            this.hdrAgName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.hdrAgName.Width = 91;
+            this.hdrOpr.DisplayIndex = 3;
+            this.hdrOpr.Text = "نوع‌عملیات";
+            this.hdrOpr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hdrOpr.Width = 100;
             // 
-            // hdrTrgtDT
+            // hdrTrg
             // 
-            this.hdrTrgtDT.Text = "تاریخ هدف";
-            this.hdrTrgtDT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.hdrTrgtDT.Width = 111;
+            this.hdrTrg.DisplayIndex = 2;
+            this.hdrTrg.Text = "تاریخ‌هدف";
+            this.hdrTrg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hdrTrg.Width = 100;
+            // 
+            // hdrUsr
+            // 
+            this.hdrUsr.DisplayIndex = 1;
+            this.hdrUsr.Text = "متصدی";
+            this.hdrUsr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.hdrUsr.Width = 100;
             // 
             // hdrDesc
             // 
+            this.hdrDesc.DisplayIndex = 0;
             this.hdrDesc.Text = "توضیحات";
             this.hdrDesc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.hdrDesc.Width = 160;
-            // 
-            // cmuListView
-            // 
-            this.cmuListView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tspRemHistItm});
-            this.cmuListView.Name = "cmuWholeForm";
-            this.cmuListView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.cmuListView.Size = new System.Drawing.Size(181, 48);
-            // 
-            // tspRemHistItm
-            // 
-            this.tspRemHistItm.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.tspRemHistItm.Name = "tspRemHistItm";
-            this.tspRemHistItm.ShortcutKeys = System.Windows.Forms.Keys.Delete;
-            this.tspRemHistItm.Size = new System.Drawing.Size(180, 22);
-            this.tspRemHistItm.Text = "حذف";
-            this.tspRemHistItm.Click += new System.EventHandler(this.tspRemHistItm_Click);
-            // 
-            // btnUpd
-            // 
-            this.btnUpd.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnUpd.Location = new System.Drawing.Point(0, 500);
-            this.btnUpd.Name = "btnUpd";
-            this.btnUpd.Size = new System.Drawing.Size(569, 36);
-            this.btnUpd.TabIndex = 1;
-            this.btnUpd.Text = "بروزرسانی";
-            this.btnUpd.UseVisualStyleBackColor = true;
-            this.btnUpd.Click += new System.EventHandler(this.btnUpd_Click);
+            this.hdrDesc.Width = 150;
             // 
             // frmRemoveOperation
             // 
-            this.AcceptButton = this.btnUpd;
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 536);
-            this.Controls.Add(this.btnUpd);
+            this.ClientSize = new System.Drawing.Size(714, 543);
             this.Controls.Add(this.lstOpr);
+            this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Samim", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "frmRemoveOperation";
-            this.Text = "لغو گردشکار";
-            this.Load += new System.EventHandler(this.frmRemoveOperation_Load);
+            this.Text = "لغو عملیات";
             this.cmuListView.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView lstOpr;
-        private System.Windows.Forms.Button btnUpd;
-        private System.Windows.Forms.ColumnHeader hdrEnt;
-        private System.Windows.Forms.ColumnHeader hdrApplyDT;
-        private System.Windows.Forms.ColumnHeader hdrAgName;
-        private System.Windows.Forms.ColumnHeader hdrTrgtDT;
-        private System.Windows.Forms.ColumnHeader hdrDesc;
         private System.Windows.Forms.ContextMenuStrip cmuListView;
-        private System.Windows.Forms.ToolStripMenuItem tspRemHistItm;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCancelItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbSearch;
+        private System.Windows.Forms.ListView lstOpr;
+        private System.Windows.Forms.ColumnHeader hdrId;
+        private System.Windows.Forms.ColumnHeader hdrTS;
+        private System.Windows.Forms.ColumnHeader hdrOpr;
+        private System.Windows.Forms.ColumnHeader hdrTrg;
+        private System.Windows.Forms.ColumnHeader hdrUsr;
+        private System.Windows.Forms.ColumnHeader hdrDesc;
     }
 }
--- src/smcs.frontend/frm/frmRemoveOperation.cs
+++ src/smcs.frontend/frm/frmRemoveOperation.cs
@@ -1,107 +1,156 @@
-﻿using smcs.backend.biz;
-using smcs.backend.data.access;
+﻿using smcs.backend.data.access;
 using smcs.backend.data.model;
+using smcs.backend.data.model.basic;
 using smcs.backend.data.model.iterative;
 using smcs.backend.data.model.parent;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Linq;
+using System.Data;
+using smcs.backend.biz;
 
 namespace smcs.frontend.frm
 {
     public partial class frmRemoveOperation : frmBase
     {
-        private void frmRemoveOperation_Load(object sender, System.EventArgs e)
-        {
-            updateUI();
-        }
-
         public frmRemoveOperation()
         {
             InitializeComponent();
         }
 
-        private void btnUpd_Click(object sender, System.EventArgs e)
+        private void cmbSearch_TextChanged(object sender, EventArgs e)
         {
-            updateUI();
-        }
-
-        private void tspRemHistItm_Click(object sender, System.EventArgs e)
-        {
-
-        }
-
-        /* ------------------ private method(es) ------------------ */
-
-        private void updateUI()
-        {
-            List<History> ls_of_his;
-            using (var rep = new Repository<History>())
-                ls_of_his = rep.RetList(h => h.Type == Crud.Create); // NOTE نیازی به عملیات‌هایی که بروزرسانی هستند، نیست؟
-
-            if (ls_of_his != null)
+            if (cmbSearch.Text.Length >= 3)
             {
-                foreach (History hi in ls_of_his)
+                loadSearchBox(cmbSearch.Text);
+
+                cmbSearch.DroppedDown = true;
+                cmbSearch.SelectionStart = cmbSearch.Text.Length;
+                cmbSearch.SelectionLength = 0;
+            }
+            else
+                cmbSearch.DroppedDown = false;
+        }
+
+        private void cmbSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                lstOpr.Items.AddRange(generateListOfLVI(((PairDataItem)cmbSearch.SelectedItem).Id).ToArray());
+
+                /* Note هم به فعال نیاز داریم و هم غیرفعال؛ شاید نیاز باشد غیرفعال را دوباره فعال کنیم */
+                var ag = new Repository<Agent>().Ret(i => i.MisRef == ((PairDataItem)cmbSearch.SelectedItem).Id);
+                var mis = new Repository<Mission>().Ret(m => m.MisId == ((PairDataItem)cmbSearch.SelectedItem).Id); 
+                var ofc = new Repository<Office>().Ret(o => o.Id == mis.OffcRef); 
+
+                MessageBox.Show("نام: " + ag.Name + "\n" + "قسمت: " + ofc.Name + "\n" + "تاریخ اعزام: " + ag.DateOfDisp.ToString("yyyy/MM/dd") + "\n" + 
+                                "تاریخ پذیرش: " + mis.InitDate.ToString("yyyy/MM/dd") + "\n" + "تاریخ معرفی‌به‌یگان: " + 
+                                (mis.Ret2UntDate.HasValue ? mis.Ret2UntDate.Value.ToString("yyyy/MM/dd") : string.Empty), "مشخصات مامور انتخابی", MessageBoxButtons.OK);
+            }
+        }
+
+        private void tsmiCancelItem_Click(object sender, EventArgs e)
+        {
+            if (lstOpr.Focused && lstOpr.SelectedItems[0] != null)
+            {
+                if (DialogResult.Yes == MessageBox.Show("عملیات انتخاب شده لغو شود؟", "", MessageBoxButtons.YesNo))
                 {
-                    if (hi.Entity == "Mission")
-                        lstOpr.Items.Add(generateLVIFromMission(hi));
-                    else
-                        lstOpr.Items.Add(generateLVIFromIterative(hi));
+                    var type = lstOpr.SelectedItems[0].SubItems[2].Text; // NOTE با اسم ستون کار نکرد
+                    var id = lstOpr.SelectedItems[0].Tag.ToString().ToInt32();
+
+                    int result = Message.FAIL;
+                    switch (type.ToEnglishName())
+                    {
+                        case "Absence":
+                            result = new BizProvider().RemoveIteration<Absence>(id).Id;
+                            break;
+                        case "OffDay":
+                            result = new BizProvider().RemoveIteration<OffDay>(id).Id;
+                            break;
+                        case "OnDuty":
+                            result = new BizProvider().RemoveIteration<OnDuty>(id).Id;
+                            break;
+                        case "UndTreat":
+                            result = new BizProvider().RemoveIteration<UndTreat>(id).Id;
+                            break;
+                        case "Mission":
+                            result = new BizProvider().ResumeMission(id).Id;
+                            break;
+                    }
+
+                    if (result == Message.SUCC)
+                    {
+                        lstOpr.Items.Remove(lstOpr.SelectedItems[0]);
+                        lstOpr.Refresh();
+                    }
                 }
             }
         }
 
-        private ListViewItem generateLVIFromMission(History hi)
+        /* ------------------ private method(es) ------------------ */
+
+        private void loadSearchBox(string filt)
         {
-            Mission mi; 
-            using (var rep = new Repository<Mission>())
-                mi = rep.Ret(a => a.MisId == hi.entId);
+            var items = new List<PairDataItem>();
 
-            Agent ag; 
-            using (var rep = new Repository<Agent>())
-                ag = rep.Ret(a => a.MisRef == mi.MisId);
+            var lst_of_proposed = new Repository<Agent>().RetList(a => a.NtioCode.Contains(filt));
+            foreach (var ag in lst_of_proposed)
+                items.Add(new PairDataItem(ag.MisRef, ag.NtioCode));
 
-            return new ListViewItem(new string[6]
-            {
-                hi.Id.ToString(),
-                hi.TimeStmp.ToString(),
-                hi.Entity, /*UNDONE نوع موجودیت باید فارسی و مناسب کاربر باشد*/
-                ag.Name,
-                mi.InitDate.ToShortDateString(),
-                mi.InitDesc
-            });
+            cmbSearch.Items.Clear();
+            cmbSearch.Items.AddRange(items.ToArray());
         }
 
-        private ListViewItem generateLVIFromIterative(History hi)
+        private List<ListViewItem> generateListOfLVI(int misId)
         {
-            Iterative it;
-            if (hi.Entity == "Absence")
-                using (var rep = new Repository<Absence>())
-                    it = rep.Ret(a => a.Id == hi.entId);
-            else if (hi.Entity == "OffDay")
-                using (var rep = new Repository<OffDay>())
-                    it = rep.Ret(a => a.Id == hi.entId);
-            else if (hi.Entity == "OnDuty")
-                using (var rep = new Repository<OnDuty>())
-                    it = rep.Ret(a => a.Id == hi.entId);
-            else if (hi.Entity == "UndTreat")
-                using (var rep = new Repository<UndTreat>())
-                    it = rep.Ret(a => a.Id == hi.entId);
-            else
-                return null; // استثنا ایجاد شود؟؟
+            var mis = new Repository<Mission>().Ret(m => m.MisId == misId && m.Enbl == true);
+            var lst_of_off = new Repository<OffDay>().RetList(i => i.MisRef == misId && i.Enbl == true);
+            var lst_of_duty = new Repository<OnDuty>().RetList(i => i.MisRef == misId && i.Enbl == true);
+            var lst_of_treat = new Repository<UndTreat>().RetList(i => i.MisRef == misId && i.Enbl == true);
+            var lst_of_abs = new Repository<Absence>().RetList(i => i.MisRef == misId && i.Enbl == true);
 
-            Agent ag; /*UNDONE اگر که پیمانه بالا تهی باشد، مامور نیز تهی می‌شود‌*/
-            using (var rep = new Repository<Agent>())
-                ag = rep.Ret(a => a.MisRef == it.MisRef);
+            var lst_of_lvi = new List<ListViewItem>();
 
-            return new ListViewItem(new string[6]
+            generateLVIForEachIteration(ref lst_of_lvi, lst_of_off.Cast<Iterative>().ToList(), "مرخصی");
+            generateLVIForEachIteration(ref lst_of_lvi, lst_of_duty.Cast<Iterative>().ToList(), "امورخدمتی");
+            generateLVIForEachIteration(ref lst_of_lvi, lst_of_treat.Cast<Iterative>().ToList(), "اعزام‌به‌بهداری");
+            generateLVIForEachIteration(ref lst_of_lvi, lst_of_abs.Cast<Iterative>().ToList(), "نهست");
+            if (mis.Ret2UntDate.HasValue)
             {
-                hi.Id.ToString(),
-                hi.TimeStmp.ToString(),
-                hi.Entity, /*UNDONE نوع موجودیت باید فارسی و مناسب کاربر باشد*/
-                ag.Name,
-                it.Date.ToShortDateString(),
-                it.Desc
-            });
+                var lvi = new ListViewItem(new string[6] { (lst_of_lvi.Count+1).ToString(), mis.TimeStmp.ToString("hh:mm:ss yyyy/MM/dd"),
+                    mis.GetType().ToPersianName() , mis.Ret2UntDate.Value.ToString("yyyy/MM/dd"), extractUser(mis.SesRef), mis.TermDesc });
+                lvi.Tag = mis.MisId;
+                lst_of_lvi.Add(lvi);
+            }
+
+            return lst_of_lvi;
+        }
+
+        private void generateLVIForEachIteration(ref List<ListViewItem> items, List<Iterative> iterations, string type)
+        {
+            foreach (var itr in iterations)
+            {
+                var item = new ListViewItem(new string[6] {
+                    (items.Count+1).ToString(),
+                    itr.TimeStmp.ToString("hh:mm:ss yyyy/MM/dd"),
+                    type,
+                    itr.Date.ToString("yyyy/MM/dd"),
+                    extractUser(itr.SesRef),
+                    itr.Desc
+                });
+
+                item.Tag = itr.Id; //NOTE توجه شود که شناسه داده بوسیله تگ استخراج می‌شود
+                items.Add(item);
+            }
+        }
+
+        private string extractUser(int sesId)
+        {
+            var rOfS = new Repository<Session>();
+            var rOfU = new Repository<User>();
+            var session = rOfS.Ret(s => s.SesId == sesId);
+            return rOfU.Ret(u => u.UsrId == session.UsrRef).Name;
         }
     }
 }
