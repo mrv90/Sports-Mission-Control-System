a7f556ee1b22f94591e97904ddd346f8fcb2b478
mrv <mrv@win-wks>
2020-01-19 16:30:07 +0330

    کلاس اینیبلر به عنوان منسوخ مارک شد.

--- src/smcs.backend/data/model/parent/Enabler.cs
+++ src/smcs.backend/data/model/parent/Enabler.cs
@@ -1,5 +1,8 @@
-﻿namespace Backend.Data.Model.Parent
+﻿using System;
+
+namespace Backend.Data.Model.Parent
 {
+    [Obsolete("References must be replaced with smcs.backend.data.model.parent.Base", true)]
     public abstract class Enabler
     {
         public bool Enbl { get; set; }
